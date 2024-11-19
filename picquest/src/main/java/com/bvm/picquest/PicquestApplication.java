e8b92e126219f33d52a20e19624a4e1c388813cb
박상학 <kaka1313@kookmin.ac.kr>
2024-11-19 10:46:18 +0900

    project init

---
+++ picquest/src/main/java/com/bvm/picquest/PicquestApplication.java
@@ -0,0 +1,13 @@
+package com.bvm.picquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicquestApplication.class, args);
+	}
+
+}
---
+++ picquest/src/test/java/com/bvm/picquest/PicquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bvm.picquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
